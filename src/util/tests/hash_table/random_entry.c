e60dab596df2eb623d739f89012eb52d5cd3a9a1
Dylan Baker <dylan@pnwbakers.com>
2018-05-22 15:00:35 -0700

    util/tests: Use define instead of VLA
    To allow the this test to be built with MSVC, which doesn't support
    VLAs.

--- src/util/tests/hash_table/random_entry.c
+++ src/util/tests/hash_table/random_entry.c
@@ -30,6 +30,8 @@
 #include <assert.h>
 #include "hash_table.h"
 
+#define SIZE 10000
+
 static uint32_t
 key_value(const void *key)
 {
@@ -53,8 +55,7 @@ main(int argc, char **argv)
 {
    struct hash_table *ht;
    struct hash_entry *entry;
-   unsigned size = 10000;
-   uint32_t keys[size];
+   uint32_t keys[SIZE];
    uint32_t i, random_value;
 
    (void) argc;
@@ -62,7 +63,7 @@ main(int argc, char **argv)
 
    ht = _mesa_hash_table_create(NULL, key_value, uint32_t_key_equals);
 
-   for (i = 0; i < size; i++) {
+   for (i = 0; i < SIZE; i++) {
       keys[i] = i;
 
       _mesa_hash_table_insert(ht, keys + i, NULL);
